b1e9a1252b43eb38bdfea0cfc803059cf7f5e3fa
root <root@KelsonPC.mshome.net>
2018-03-20 20:58:20 -0300

    ok

--- site/weblogic/manifests/cfg_arquivos.pp
+++ site/weblogic/manifests/cfg_arquivos.pp
@@ -1,18 +1,4 @@
-#Classe de confi    exec { 'JAVA_HOME':
-    environment => ["JAVA_HOME=${weblogic::java_env_path}"],
-  }
-  exec { 'MW':
-    environment => ["MW=${weblogic::mw_env_path}"],
-  }
-  exec { 'WL_LOG':
-    environment => ["WL_LOG=${weblogic::wl_log_env_path}"],
-  }
-  exec { 'DOMAINS':
-    environment => ["DOMAINS=${weblogic::domains_env_path}"],
-  }
-  exec { 'PATH':
-    environment => ["PATH=\$JAVA_HOME/bin:\$PATH:\$HOME/bin"],
-  }guração de arquivos
+#Classe de configuração de arquivos
 class weblogic::cfg_arquivos inherits weblogic {
 
   file { $weblogic::oracle_dir:
