5d4448505ac2618a23999e77d741043aafe73247
root <root@KelsonPC.mshome.net>
2018-03-22 18:15:14 -0300

    alterar tunning

--- modules/weblogic/manifests/tunning_weblogic.pp
+++ modules/weblogic/manifests/tunning_weblogic.pp
@@ -5,7 +5,7 @@ class weblogic::tunning_weblogic{
   notify { 'msg-tunning-weblogic':
     message => "====>\tEfetuando tunning do Weblogic",
   }
-# /opt/Oracle/java
+  #/opt/Oracle/java
   #file { "${lookup('weblogic::env_java_home')}/jre/lib/security":
   file { '/workspace/teste':
     ensure => present,
@@ -22,4 +22,4 @@ class weblogic::tunning_weblogic{
 #      content => 'MEM_ARGS="-Djava.security.egd=file:/dev/./urandom"; export MEM_ARGS',
 #    }
 #  }
-#}
+}
