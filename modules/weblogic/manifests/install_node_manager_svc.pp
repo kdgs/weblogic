8b1e6dddc5cfe32d0266fcbd112aac1613dc58ad
Kelson Daniel Goncalves da Silva <kelsong@gmail.com>
2018-03-22 18:48:46 -0300

    configura node manager

--- modules/weblogic/manifests/install_node_manager_svc.pp
+++ modules/weblogic/manifests/install_node_manager_svc.pp
@@ -5,14 +5,14 @@ class weblogic::install_node_manager_svc {
   class weblogic::install_java {
     notify { 'msg-install-node-manager':
       message => "====>\tIniciando configuração do nodeManagerDaemon",
-  }
+    }
   #chmod +x /etc/init.d/startNodeDaemon
   #file { '/etc/init.d/startNodeManagerDaemon.sh':
-  file { '/workspace/teste.sh':
-    source  => 'puppet:///modules/startNodeManagerDaemon.sh',
-    replace => false,
-    mode    => '0111',
-  }
+    file { '/workspace/teste.sh':
+      source  => 'puppet:///modules/startNodeManagerDaemon.sh',
+      replace => false,
+      mode    => '0111',
+    }
   #exec { 'chkconfig −−add startNodeDaemon':
   #  path    => ['/usr/bin', '/usr/sbin',],
   #}
